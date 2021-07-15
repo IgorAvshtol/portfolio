d5b119767001c53267c845ec62c5cecd3ef9ddc5
IgorAvshtol <iharrwe@gmail.com>
2021-07-15 22:41:18 +0300

    add reval typing

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,12 +3,10 @@ import style from './Nav.module.scss';
 const Nav = () => {
     return (
         <div className={style.nav}>
-
             <a href="">Main</a>
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
             <a href="">Contacts</a>
-
         </div>
     );
 }
